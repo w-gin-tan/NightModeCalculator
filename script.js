955bbc0760a869693a2919e48e7ab11b157b0b85
w-gin-tan <wtg28@outlook.com.au>
2023-02-21 16:22:29 +0800

    feat: Implement operation logic to math functions.
    Simple operation logic by returning operations from function params.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const add = () => {
-
+const add = (a, b) => {
+    return a + b;
 }
 
-const subtract = () => {
-
+const subtract = (a, b) => {
+    return a - b;
 }
 
-const multiply = () => {
-
+const multiply = (a, b) => {
+    return a * b;
 }
 
-const divide = () => {
-    
+const divide = (a, b) => {
+    return a / b;
 }

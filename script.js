5b6c8413e2b0821f3ec5bafbb7a273878532cbab
w-gin-tan <wtg28@outlook.com.au>
2023-02-22 14:58:33 +0800

    feat: Make a function that handles operation calls
    Let one function handle all operation calls instead of separating them.

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 }
+
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case "add": add(a, b); break;
+        case "subtract": subtract(a, b); break;
+        case "multiply": multiply(a, b); break;
+        case "divide": divide(a, b); break;
+    }
+}

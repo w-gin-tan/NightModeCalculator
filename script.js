5263183c7d171388336faac050037c5602a80d28
w-gin-tan <wtg28@outlook.com.au>
2023-06-03 00:27:43 +0800

    fix: Prevent init zero dups & fix 'equals' logic

--- script.js
+++ script.js
@@ -33,17 +33,32 @@ const displayValue = (val) => {
 const numSetup = () => {
     let val = "";
     const nums = document.querySelectorAll(".nums");
+    const sign = document.querySelector("#sign"); 
+    const decimal = document.querySelector("#decimal");
+    const display = document.getElementById("ans");
+
+    /*
+    sign.addEventListener("click", (e) => {
+        // remove 0 at some point when switching to negative number
+        val = val.includes("-") ? val.slice(1) : "-" + val;
+        displayValue(val);  
+    });
+    */
 
     nums.forEach((num) => {
         num.addEventListener("click", (e) => {
-            val += num.textContent;
-            displayValue(val);
-        })
+            if (display.textContent === "0" && num.textContent === "0") {
+                displayValue(0); // no duplicates of zero to add if the result is zero
+            } else {
+                val += num.textContent;
+                displayValue(val);
+            }
+        });
     });
 }
 
 const operSetup = () => {
-    let left, right, operator;
+    let left, right, operator, result;
 
     // operation and equals sign keys
     const operations = document.querySelectorAll(".operations");
@@ -51,21 +66,24 @@ const operSetup = () => {
 
     // calculator display
     const display = document.getElementById("ans");
-    display.textContent = "0";
 
     numSetup();
 
     operations.forEach((operation) => {
         operation.addEventListener("click", (e) => {
             left = parseInt(display.textContent);
-            operator = operation.id;     
+            operator = operation.id;
+            right = null;     
             numSetup(); // reload expression for rhs of operation
         });
     });
 
     equals.addEventListener("click", (e) => {
-        right = parseInt(display.textContent);
-        display.textContent = operate(operator, left, right);
+        if (!right && left) {
+            right = parseInt(display.textContent);
+            result = operate(operator, left, right);
+            displayValue(result);
+        }
         numSetup();
     });
 }
